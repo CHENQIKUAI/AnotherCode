d43b2806930fe0d048d99210df4eea1b5eab0ec1
CHENQIKUAI <1779299572@qq.com>
2018-03-22 16:27:27 +0800

    add annother funny thing.

---
+++ freeProperly.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "stdlib.h"
+#include "string.h"
+int main() {
+	char *name = (char*)malloc(strlen("Susan") + 1);
+	strcpy(name, "Susan");
+	while (*name != '\0') {
+		printf("%c", *name);
+		name++;
+	}
+
+
+	free(name);//When free is writing, the pogram does not work.Because the pointer name is the point to the start.
+	//If we can get the start address, we can free the pointer successfully! :)
+
+
+	/*name--;
+		name--;
+		name--;
+		name--;
+		name--;
+		free(name);*/
+	return 0;
+}
